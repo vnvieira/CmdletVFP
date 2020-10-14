7e80ecab2edcf0fdae79d7bd8a3f8186e03619b1
Vinicius Novak <vinicius_novak@hotmail.com>
2020-10-13 21:39:55 -0300

    Add project files.

---
+++ CmdletVFP/CmdletVFP.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Management.Automation;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[Cmdlet(VerbsCommon.Get, "VFP")]
+public class VisualFoxProRun : Cmdlet
+{
+    [Parameter(Mandatory = true)]
+    public string FileName { get; set; }
+
+    [Parameter(Mandatory = false)]
+    public string Options { get; set; }
+
+    protected override void ProcessRecord()
+    {
+        object vfp, response;
+        Type type;
+
+        type = Type.GetTypeFromProgID("XVFP.XVFP");
+        vfp = Activator.CreateInstance(type);
+
+        object[] args = { Path.Combine(Directory.GetCurrentDirectory(), Path.ChangeExtension(FileName, ".prg")), Options };
+
+        response = type.InvokeMember("run", BindingFlags.InvokeMethod, null, vfp, args);
+
+        Marshal.ReleaseComObject(vfp);
+
+        WriteObject(response);
+    }
+}
